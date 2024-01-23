b1b4fcd0c2fa2eb71194f1ca2eb84ca153b5c0d6
ashleevo <ashleevo@Ashlees-MacBook-Pro.local>
2024-01-22 20:23:56 -0800

    pushing new files

---
+++ ETL Mini Project Code/crowdfunding_db.sql
@@ -0,0 +1,7 @@
+-- ensuring all the data has loaded into the respective tables
+SELECT * FROM category
+SELECT * FROM subcategory
+SELECT * FROM contacts
+SELECT * FROM campaign
+
+-- *note* they all loaded in successfully
---
+++ ETL Mini Project Code/crowdfunding_schema.sql
@@ -0,0 +1,44 @@
+-- creating contacts table
+CREATE TABLE contacts (
+    contact_id INT NOT NULL,
+    first_name VARCHAR (50) NOT NULL,
+    last_name VARCHAR (50) NOT NULL,
+    email VARCHAR (150) NOT NULL,
+	PRIMARY KEY (contact_id)
+);
+
+-- creating category table
+CREATE TABLE category (
+    category_id VARCHAR (50) NOT NULL,
+    category VARCHAR (50) NOT NULL,
+	PRIMARY KEY (category_id)
+);
+
+-- creating subcategory table
+CREATE TABLE subcategory (
+    subcategory_id VARCHAR (50) NOT NULL,
+    subcategory VARCHAR (50) NOT NULL,
+	PRIMARY KEY (subcategory_id)
+);
+
+-- creating campaign table
+CREATE TABLE campaign (
+    cf_id INT NOT NULL,
+    contact_id INT NOT NULL,
+    company_name VARCHAR (150) NOT NULL,
+    description VARCHAR (150) NOT NULL,
+    goal FLOAT NOT NULL,
+    pledged FLOAT NOT NULL,
+    outcome VARCHAR (50) NOT NULL,
+    backers_count INT NOT NULL,
+    country VARCHAR (50) NOT NULL,
+    currency VARCHAR (50) NOT NULL,
+    launched_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    category_id VARCHAR (50) NOT NULL,
+    subcategory_id VARCHAR (50) NOT NULL,
+	PRIMARY KEY (cf_id),
+	FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
+	FOREIGN KEY (category_id) REFERENCES category(category_id),
+	FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id)
+);
